4479aa305b46d5980c10ecf9dccd9b005bff58c0
Krisha Botadara <krishabotadara@gmail.com>
2024-04-19 09:10:10 +0700

    Resolved conflicts

--- src/app/components/Home.tsx
+++ src/app/components/Home.tsx
@@ -1,40 +1,19 @@
-<<<<<<< HEAD
-import Navbar from "./Navbar"
+import Navbar from "./Navbar";
 import HeroVideo from "./HeroVideo";
-import FilterJobs from "./search"
-import Introduction from "./Introduction"
-import SearchJob from "./jobs"
-import Footer from "./Footer"
+import FilterJobs from "./search";
+import Introduction from "./Introduction";
+import SearchJob from "./jobs";
+import Footer from "./Footer";
 
-export default function Home(){
-    return(
-        <div>
-            <Navbar />
-            <HeroVideo />
-            <FilterJobs />
-            <Introduction />
-            <SearchJob />
-            <Footer />
-        </div>
-    )
-=======
-import Navbar from "./Navbar"
-import HeroVideo from "./HeroVideo";
-import FilterJobs from "./search"
-import Introduction from "./Introduction"
-import SearchJob from "./jobs"
-import Footer from "./Footer"
-
-export default function Home(){
-    return(
-        <div>
-            <Navbar />
-            <HeroVideo />
-            <FilterJobs />
-            <Introduction />
-            <SearchJob />
-            <Footer />
-        </div>
-    )
->>>>>>> c2c542d521f833fa6787f58400f185eaba5d564b
+export default function Home() {
+  return (
+    <div>
+      <Navbar />
+      <HeroVideo />
+      <FilterJobs />
+      <Introduction />
+      <SearchJob />
+      <Footer />
+    </div>
+  );
 }
